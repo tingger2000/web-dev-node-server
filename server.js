c10e61538aa6379fa4538b53c636e0c2bb82a86c
tingger2000 <yuting.ger@gmail.com>
2022-04-06 18:01:37 -0400

    update database user

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb+srv://tingger2000:tingtingger518@cluster0.nzwnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://webdev:webdevpassword@cluster0.nzwnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 //mongoose.connect('mongodb://localhost:27017/webdev');
 
 const app = express();
