bda28dd457752879c14c77022dbe23f74799cacf
tingger2000 <yuting.ger@gmail.com>
2022-04-06 16:43:09 -0400

    a9 MONGO sp22

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/webdev');
 
 const app = express();
 app.use(cors());
